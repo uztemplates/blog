5387f3a1b5a91e4c23f45f10ed37ac51ca770d50
uztemplates <92620770+uztemplates@users.noreply.github.com>
2021-10-30 10:30:48 +0500

    Add files via upload

---
+++ responsive yana/js/script.js
@@ -0,0 +1,44 @@
+// var about = [
+//  ["Karim", "Asensio", "Junior"],
+//  ["Benzema", "Marco", "Vinisius"],
+//  ["Fransiya", "Ispaniya", "Braziliya"]
+// ]
+// console.log(about[1][1] + ' ' + about[0][1] + ' ' + about[2][1] + 'da yashaydi');
+
+// --------------------------------------------
+
+// var about = {
+//  ism: "Asensio",
+//  fam: "Marco",
+//  manzil: "Ispnaiya"
+// }
+// console.log(about.fam + " " + about.ism + " " + about.manzil + "da yashaydi");
+
+// -------------------------------------------
+
+// var nimadir = {
+//  ism: "Asensio",
+//  fam: "Marco",
+//  manzil: "Madrid",
+//  klub: ["Real Madrid"]
+// };
+// console.log(nimadir.fam + " " + nimadir.ism + " " + nimadir.manzil + "da yashaydi. Hozirda " + nimadir.klub[0] + " klubida o'ynaydi");
+
+// -------------------------------------------
+
+// var qiziqish = {
+//  ism: "Diyorbek",
+//  fam: "Marco",
+//  manzil: "Madrid",
+//  music: [
+//  {
+//   name: "BadBoy",
+//   janr: "rap",
+//  },
+//  {
+//   name: "Radius 21",
+//   janr: "rap",
+//  }
+// ]
+// };
+//  console.log(`${qiziqish.ism} ${qiziqish.music[0].janr} janrida ijod qiluvchi ${qiziqish.music[0].name} treklarini tinglaydi`);
